a2ab363c59a92f54449624fe3a3dbd3477941828
Marco Ollivier <marcopollivier@gmail.com>
2022-10-16 06:06:23 -0300

    first commit

---
+++ test/clj_simple_icons/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-simple-icons.core-test
+  (:require [clojure.test :refer :all]
+            [clj-simple-icons.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
